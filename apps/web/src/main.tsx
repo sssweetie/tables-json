55948d60c073f4897ddbf07303dbd40c3f2ed8cb
sweetie <63711180+sssweetie@users.noreply.github.com>
2024-05-21 01:27:00 +0300

    fix: hashRouter

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,6 @@
 import '@elastic/eui/dist/eui_theme_light.css';
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { Posts } from './features/Posts';
@@ -11,21 +11,15 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Posts />,
-  },
-  {
-    path: '/details/:id',
-    element: <Details />,
-  },
-]);
-
 root.render(
   <StrictMode>
     <EuiProvider colorMode="light">
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" Component={Posts}></Route>
+          <Route path="/details/:id" Component={Details}></Route>
+        </Routes>
+      </HashRouter>
     </EuiProvider>
   </StrictMode>
 );
